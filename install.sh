1597a1a7c1bd5012f8f2dda287ae1fc03491cfd5
fengling520xu <42179576+fengling520xu@users.noreply.github.com>
2026-07-11 01:09:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 APP_NAME="X-MILI"
-REPO="https://github.com/Aimilibot/X-MILI"
+REPO="https://github.com/fengling520xu/X-MILI"
 RELEASE_TAG="${X_MILI_RELEASE_TAG:-latest}"
 INSTALL_DIR="${XUI_MAIN_FOLDER:-/usr/local/x-ui}"
 DATA_DIR="/etc/x-ui"
